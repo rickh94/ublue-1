866d86f593e8c876219c7db5e35ce5ef678bf5b4
Rick Henry <rickhenry@rickhenry.dev>
2026-04-29 16:51:39 -0400

    ugh
    Signed-off-by: Rick Henry <rickhenry@rickhenry.dev>

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,7 +33,6 @@ dnf5 install -y nix
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
-<<<<<<< HEAD
 # systemctl disable gdm || true
 # systemctl disable sddm || true
 systemctl disable plasmalogin || true
