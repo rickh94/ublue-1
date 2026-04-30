14cc4f4a6e8b9683b81ec949403c2d0ac5b1763f
Rick Henry <rickhenry@rickhenry.dev>
2026-04-30 17:31:02 -0400

    switch from dms to noctalia

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,10 +22,11 @@ set -ouex pipefail
 dnf5 install -y nix-core cockpit cockpit-machines helix gcc llvm chezmoi rclone
 dnf5 -y copr enable ryanabx/cosmic-epoch
 dnf5 -y copr enable yalter/niri
-dnf5 -y copr enable avengemedia/dms
+# dnf5 -y copr enable avengemedia/dms
 dnf5 -y copr enable scottames/ghostty
+dnf5 install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf5 install -y cosmic-desktop 
-dnf5 install -y niri dms mako dms-greeter fuzzel alacritty dsearch cava
+dnf5 install -y niri mako fuzzel noctalia-shell upower cliphist evolution-data-server
 dnf5 install -y ghostty
 dnf5 -y copr disable ryanabx/cosmic-epoch
 dnf5 -y copr disable avengemedia/dms
