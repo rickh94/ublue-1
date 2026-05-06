f106ce59c5caab29c85c35a1985eaeb4b1bafc03
Rick Henry <rickhenry@rickhenry.dev>
2026-05-06 00:10:27 -0400

    remove selinux nonsense

--- build_files/build.sh
+++ build_files/build.sh
@@ -36,7 +36,7 @@ dnf5 -y copr disable yalter/niri
 
 wget -O /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
 chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
-chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+#chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
 
 #### Example for enabling a System Unit File
 
