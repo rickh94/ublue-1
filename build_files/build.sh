3f9fcbafc4f04e603f08b4176f3360cfce235517
Rick Henry <rickhenry@rickhenry.dev>
2026-05-05 23:59:32 -0400

    remove cosmic dekstop, add bitwarden policy

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,7 +25,8 @@ dnf5 -y copr enable ryanabx/cosmic-epoch
 dnf5 -y copr enable yalter/niri
 dnf5 -y copr enable scottames/ghostty
 
-dnf5 install -y cosmic-desktop 
+# dnf5 install -y cosmic-desktop 
+dnf5 install -y cosmic-greeter
 dnf5 install -y niri mako fuzzel
 dnf5 install -y ghostty
 
@@ -33,6 +34,9 @@ dnf5 -y copr disable ryanabx/cosmic-epoch
 dnf5 -y copr disable scottames/ghostty
 dnf5 -y copr disable yalter/niri
 
+sudo wget -O /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
+sudo chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+sudo chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
 
 #### Example for enabling a System Unit File
 
