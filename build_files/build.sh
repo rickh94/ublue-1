c06ee19cfe11988179d01a4794037b4ed46ebf34
Rick Henry <rickhenry@rickhenry.dev>
2026-05-06 00:05:05 -0400

    fix dumb sudo thing

--- build_files/build.sh
+++ build_files/build.sh
@@ -34,9 +34,9 @@ dnf5 -y copr disable ryanabx/cosmic-epoch
 dnf5 -y copr disable scottames/ghostty
 dnf5 -y copr disable yalter/niri
 
-sudo wget -O /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
-sudo chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
-sudo chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+wget -O /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
+chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
 
 #### Example for enabling a System Unit File
 
